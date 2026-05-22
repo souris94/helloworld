5e897c0bb8e8d0b112d724d904bac434ac24535a
Yannis Desire <souris93290@hotmail.fr>
2026-05-22 12:20:30 +0100

    add new file and modified app.js

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-console.log("hello world")
+/* console.log("hello world")
 console.log("Yannis")
 console.log("Marcelo")
+console.log(10 + 5)
+console.log("10 + 5") */
+
+/* this is just a comment */
+
+let nom = "Yannis"
+
+console.log(nom)
+
+let nom2 = "Marcelo"
+
+console.log(nom2)
---
+++ exercice1.js
@@ -0,0 +1,5 @@
+let nom = "Yannis"
+let age = "36"
+
+console.log("my name is " + nom)
+console.log("my age is " + age)
